d5e0508b96cd4576418dd9e732f6f948293d21bc
Javier <737907@unizar.es>
2020-03-10 00:55:20 +0100

    -aMejorado el formato JSON y verificacion del JSON Schema

--- TFG/venv/main_functions.py
+++ TFG/venv/main_functions.py
@@ -1,7 +1,21 @@
-from zat import bro_log_reader
 import numpy as np
 import json
-from collections import defaultdict
+from jsonschema import *
+
+schema = {
+    "type" : "object",
+    "properties":{
+        "IP": {"type" : "string"},
+        "OS": {"type" : "array"},
+        "Services":{"type": "array"},
+        "Software":{"type":"array"}
+    },
+    "required": ["IP", "OS", "Services"]
+}
+
+def validate_and_add(item,schema,list):
+  validate(item, schema=schema)
+  list.append(item)
 
 def unique(list1):
     # insert the list to the set
@@ -13,53 +27,101 @@ def unique(list1):
         final_list.append(str(np.unique(x))[1:-1].replace("'", ""))
     return final_list
 
-def leer(rutatolog):
-    reader = bro_log_reader.BroLogReader(rutatolog)
-    listahosts=[]
-    for row in reader.readrows():
-        listahosts.append(row['id.orig_h'])
+def readips(rutatolog):
+    reader=open(rutatolog, 'r')
+    listahosts = []
+    for line in reader:
+        jsonreader = json.loads(line)
+        listahosts.append(jsonreader['id.orig_h'])
     listaips=unique(listahosts)
     return listaips
+
 def knownservices(ips,rutatolog):
-    reader = bro_log_reader.BroLogReader(rutatolog)
+
+    #Leemos el json de los known_services
+    reader = open(rutatolog, 'r')
+    lines=[]
+    for line in reader:
+        row = json.loads(line)
+        lines.append(row)
+
+    # Para cada una de las lineas, se busca si coincide la IP y se registran los servicios
     finallist=[]
     for x in ips:
+
         serviceslist=[]
         ipandservice=[]
-        for row in reader.readrows():
-            if row['host']==x:
-                print(str(row['service'])+": "+str(x))
-                serviceslist.append(str(row['service']))
+        for row in lines:
+            if row['host'] == str(x):
+                if(len(str(row['service']))<100): #Para eliminar basura
+                    serviceslist.append(row['service'])
         ipandservice=[x,serviceslist]
         finallist.append(ipandservice)
     return finallist
+
 def software(ips,rutatolog):
-    reader = bro_log_reader.BroLogReader(rutatolog)
+    reader = open(rutatolog, 'r')
+    lines = []
+    for line in reader:
+        row = json.loads(line)
+        lines.append(row)
+
+
     finallist = []
     finallist2=[]
     for x in ips:
         softwarelist = []
         oslist = []
         ipsandos = []
-        ipandsoftware = []
-        for row in reader.readrows():
+        ipsandsoftware = []
+        for row in lines:
             if row['host'] == x:
                 if ("ubuntu" in str(np.unique(str(row['unparsed_version']))))or ("Ubuntu" in str(np.unique(str(row['unparsed_version'])))) or ("Debian" in str(np.unique(str(row['unparsed_version'])))):
                     oslist.append("Linux")
                 elif ("Windows" in str(np.unique(str(row['unparsed_version'])))):
                     oslist.append("Windows")
-
+                else:
+                    oslist.append("Unknown")
                 softwarelist.append(str(np.unique(str(row['unparsed_version']))))
-        ipandsoftware=[x,softwarelist]
-        ipandos=[x,oslist]
-        finallist.append(ipandsoftware)
-        finallist2.append(ipandos)
+
+        ipsandsoftware=[x,softwarelist]
+        ipsandos=[x,oslist]
+        finallist.append(ipsandsoftware)
+        finallist2.append(ipsandos)
     return finallist,finallist2
+
 dir = input("Introduce the base directory where the logs are: ")
-listaips=leer(dir+'/conn.log')
-listaservicios=knownservices(listaips,dir+'/known_services.log')
-listasoftware,listasos=software(listaips,'/software.log')
-#/home/javier/Escritorio/prueba
-final=json.dumps(listaservicios,indent=2)
-parseado2=json.dumps(listasoftware,indent=2)
+listaips=readips(dir+'/home/javier/Escritorio/prueba2/conn.log')
+listaservicios=knownservices(listaips,dir+'/home/javier/Escritorio/prueba2/known_services.log')
+listasoftware,listasos=software(listaips,dir+'/home/javier/Escritorio/prueba2/software.log')
+print(listasos)
+
+network=[]
+for ip in listaips:
+    for dev in listasos:
+        if dev[0]==ip:
+            os=dev[1]
+            print(os)
+            if(("Unknown" in dev[1])and (len(dev[1])>1)):
+                os=dev[1].remove("Unknown")
+            if(os==None):
+                os=["Unknown"]
+
+
+    for dev in listaservicios:
+        if dev[0]==ip:
+            services=dev[1]
+    for dev in listasoftware:
+        if dev[0]==ip:
+            software=dev[1]
+    active={
+        "IP":ip,
+        "OS":os,
+        "Services":services,
+        "Software":software
+    }
+    validate_and_add(active,schema,network)
+
+print(network)
+
 exit()
