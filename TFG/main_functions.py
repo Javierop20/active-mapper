4a5546ffed78282539f0c6c5bdb9f909bed27210
Javier <jortegapal1998@gmail.com>
2020-04-20 23:15:23 +0200

    Cambios en las funcionalidades

--- TFG/main_functions.py
+++ TFG/main_functions.py
@@ -31,6 +31,14 @@ def unique(list1):
         final_list.append(str(np.unique(x))[1:-1].replace("'", ""))
     return final_list
 
+def cleanname(name):
+    name = str(name).replace('""', '')
+    name = str(name).replace('""', '')
+    name = str(name).replace('"', '')
+    name = str(name).replace("#", '')
+    name = str(name).replace(";", '')
+    return name
+
 def readips(rutatolog):
     reader=open(rutatolog, 'r')
     listahosts = []
@@ -90,11 +98,7 @@ def software(ips,rutatolog):
             for row in lines:
                 if row['host'] == x:
                     name=row['unparsed_version']
-                    name = str(name).replace('""', '')
-                    name = str(name).replace('"','')
-                    name = str(name).replace("#", '')
-                    name = str(name).replace(";", '')
-                    name=str(name)
+                    name=cleanname(name)
                     if ("ubuntu" in str(np.unique(str(row['unparsed_version']))))or ("Ubuntu" in str(np.unique(str(row['unparsed_version'])))) or ("Debian" in str(np.unique(str(row['unparsed_version'])))):
                         oslist.append("Linux")
                     elif ("Windows" in str(np.unique(str(row['unparsed_version'])))):
@@ -193,7 +197,7 @@ def ja3lookup(ja3fing):
         listuseragent = response.json()
         for item in listuseragent:
             if 'User-Agent' in item:
-                useragents.append(item['User-Agent'])
+                useragents.append(cleanname(item['User-Agent']))
     return useragents
 
 def writedata(rutatolog,data):
@@ -217,7 +221,7 @@ cwd=os.getcwd()
 os.system("figlet Active-mapper")
 print("Introduce the base directory where the pcap is located: ")
 dir=sys.stdin.readline().strip()
-os.system("export PATH=/opt/zeek/bin:$PATH && cd "+dir+" && zeek -Cr "+dir+"*.pcap local")
+os.system("export PATH=/usr/local/zeek/bin:$PATH && cd "+dir+" && zeek -Cr "+dir+"*.pcap local")
 os.system("p0f -r "+dir+"*.pcap -o "+dir+"p0f.log > /dev/null")
 
 listaips=readips(dir+'conn.log')
@@ -228,38 +232,46 @@ p0f = p0freader(listaips,dir+'p0f.log')
 
 network=[]
 for ip in listaips:
-    for dev in listasos:
-        if dev[0]==ip:
-            operative=unique(dev[1])
-    for dev in p0f:
-        if dev[0]==ip:
-            operative=unique(dev[1])
-    for dev in listaservicios:
-        if dev[0]==ip:
-            services=dev[1]
-    for dev in listasoftware:
-        if dev[0]==ip:
-            software=dev[1]
-    for dev in ja3:
-        if dev[0]==ip:
-            ja3fing=dev[1]
-    for dev in ja3:
-        if dev[0]==ip:
-            ja3fing=dev[1]
-            useragents=ja3lookup(ja3fing)
+    if ip not in ['255.255.255.255','0.0.0.0','::']:
+        for dev in listasos:
+            if dev[0]==ip:
+                operative=unique(dev[1])
+        for dev in p0f:
+            if dev[0]==ip:
+                operative=unique(dev[1])
+        for dev in listaservicios:
+            if dev[0]==ip:
+                services=dev[1]
+        for dev in listasoftware:
+            if dev[0]==ip:
+                software=dev[1]
+        for dev in ja3:
+            if dev[0]==ip:
+                ja3fing=dev[1]
+        for dev in ja3:
+            if dev[0]==ip:
+                ja3fing=dev[1]
+                useragents=ja3lookup(ja3fing)
 
-    active={
-        "IP":ip,
-        "OS":operative,
-        "Services":services,
-        "Software":software,
-        "JA3_Fingeprint":ja3fing,
-        "Possible User-Agent":useragents
-    }
-    validate_and_add(active,schema,network)
-    #print(active)
+        active={
+            "IP":ip,
+            "OS":operative,
+            "Services":services,
+            "Software":software,
+            "JA3_Fingeprint":ja3fing,
+            "Possible User-Agent":useragents
+        }
+        validate_and_add(active,schema,network)
+
 writedata(dir,network)
 os.system("cat "+dir+"data.json > "+cwd+"/templates/data.json")
-
-
-
+tmp = os.popen("ls "+dir+"*.pcap").read()
+command ="./brassfork -nodes="+dir+"nodes.csv -edges="+dir+"edges.csv"+" -in="+tmp
+os.system("cd "+cwd+"&& "+command)
+os.system("sed 's/,/"+"\t"+"/g' "+dir+"nodes.csv"+" > "+dir+"finalnodes.csv")
+os.system("sed 's/,/"+"\t"+"/g' "+dir+"edges.csv"+" > "+dir+"finaledges.csv")
+os.system("tr '[:upper:]' '[:lower:]' <"+dir+"finaledges.csv >"+dir+"outputedges.csv")
+os.system("tr '[:upper:]' '[:lower:]' <"+dir+"finalnodes.csv >"+dir+"outputnodes.csv")
+os.system("python3 convCSVtoGEXF.py -n "+dir+"outputnodes.csv -e "+dir+"outputedges.csv -d "+cwd+"/definitions.txt")
+os.system("mv "+cwd+"/out.gexf "+dir)
+os.system("rm "+dir+"*.csv")
