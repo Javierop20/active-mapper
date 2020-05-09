66cb568d31a0b8ba2c9c0da0836e9fb41e5b9b9c
Javier <jortegapal1998@gmail.com>
2020-05-09 15:26:07 +0200

    Añadido wget a la lista de cosas a instalar

--- TFG/install.sh
+++ TFG/install.sh
@@ -2,8 +2,7 @@
 echo Welcome to the Active-mapper installation. This will require sudo privileges
 dir=$PWD
 echo Installing Zeek and dependencies...
-sudo apt-get install flex bison libpcap-dev libssl-dev python-dev swig zlib1g-dev git figlet -y
-sudo apt-get install -y p0f
+sudo apt-get install wget p0f flex bison libpcap-dev libssl-dev python-dev swig zlib1g-dev git figlet -y
 sudo sh -c 'echo deb http://download.opensuse.org/repositories/security:/zeek/xUbuntu_18.04/ / > /etc/apt/sources.list.d/security:zeek.list'
 wget -nv https://download.opensuse.org/repositories/security:zeek/xUbuntu_18.10/Release.key -O Release.key
 sudo apt-key add - < Release.key
